d1f173517467d64df2715f305e1087f553fa02f2
Urtzi <urcrmedev@gmail.com>
2023-11-15 10:17:12 +0100

    ADD Comida model

---
+++ src/net/urtzi/examen/models/Comida.java
@@ -0,0 +1,21 @@
+package net.urtzi.examen.models;
+
+public class Comida {
+	private String codigo;
+	private String nombre;
+	private double precio;
+	private boolean disponible;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+}
