4fa31ed4b3339f3a007261aa19615ad822269859
Urtzi <urcrmedev@gmail.com>
2023-11-15 11:30:36 +0100

    ADD mostrarVentanaEmergente method and implemented about functionality

--- src/net/urtzi/examen/controller/ProductoController.java
+++ src/net/urtzi/examen/controller/ProductoController.java
@@ -10,11 +10,13 @@ import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -100,7 +102,7 @@ public class ProductoController implements javafx.fxml.Initializable {
 
     @FXML
     void onAboutClicked(ActionEvent event) {
-    	
+    	mostrarVentanaEmergente("INFO", "GEstion de productos V0.1\n Autor: Urtzi", AlertType.INFORMATION);
     }
 
     @FXML
@@ -128,6 +130,15 @@ public class ProductoController implements javafx.fxml.Initializable {
 			else
 				tc.setCellValueFactory(new PropertyValueFactory<>("disponible"));
 		}
+	}
+	
+
+	private static void mostrarVentanaEmergente(String titulo, String content, AlertType tipo) {
+		Alert anadidoAnimal = new Alert(tipo);
+		anadidoAnimal.setTitle(titulo);
+		anadidoAnimal.setHeaderText(null);
+		anadidoAnimal.setContentText(content);
+		anadidoAnimal.showAndWait();
 	}
 
 
